6b881bc6ab1636269bb194d06a59bbf80e96cfc0
Denter1gg <dentop1gg.yt@gmail.com>
2023-10-30 21:09:13 +0100

    Final

---
+++ example/todo/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .views import RegisterUser, LoginUser
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path ('', views.home_view, name='home'),
+    path('about', views.to_about, name='about'),
+    path('delete_task/<int:task_id>/', views.delete_task_view, name='delete_task'),
+    path('create_post/', views.post_create, name='create_post'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('category/<int:cat_id>/', views.category_posts, name='category_posts'),
+
+]
